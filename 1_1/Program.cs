0011a078c8800fe45d6faa9ec50f19204f5a6c84
Владислав <vladuk2332@gmail.com>
2025-10-01 19:24:31 +0300

    Добавьте файлы проекта.

---
+++ 1_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    internal class Program
+    {    
+        static void Main(string[] args)
+        {
+            for(; ; )
+            {
+                Console.WriteLine("Введи 1-е число");
+                try
+                {
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введи 2-е число");
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введи операцию(+,-,/,*)");
+                    char operation = char.Parse(Console.ReadLine());
+                    switch (operation)
+                    {
+                        case '+':Console.WriteLine($"Результат{a}+{b}={a + b}");
+                            break;
+                        case '-':Console.WriteLine($"Результат{a}-{b}={a - b}");
+                            break;
+                        case '*':Console.WriteLine($"Результат{a}*{b}={a * b}");
+                            break;
+                        case '/':Console.WriteLine($"Результат{a}/{b}={a / b}");
+                            break;
+                        default: throw new Exception();
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Делить на ноль нельзя!!");
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка ввода данных!");
+                }
+
+            }
+        }
+    }
+}
